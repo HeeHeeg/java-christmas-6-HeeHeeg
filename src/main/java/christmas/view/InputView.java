5ed7dc639947e88567d8fe234c7e100f48b35c76
HeeHeeg <qmffndoden@naver.com>
2023-11-10 21:35:17 +0900

    feat(christmas): 1 이상 31 이하의 숫자가 아닌 경우 예외처리 기능 구현-방문 날짜를 입력받는 기능 완성.

--- src/main/java/christmas/view/InputView.java
+++ src/main/java/christmas/view/InputView.java
@@ -1,17 +1,23 @@
 package christmas.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import christmas.validation.InputValidation;
 
 public class InputView {
+    private static final InputValidation inputValidation = new InputValidation();
+
     public int readDate() {
         System.out.println("12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)");
         boolean validInputNumber = false;
         int inputNumber = 0;
         while (!validInputNumber) {
-            String input = Console.readLine();
-            inputNumber = Integer.parseInt(input);
-            if (!(inputNumber >= 1 && inputNumber <= 31)) {
+            try {
+                String input = Console.readLine();
+                inputNumber = Integer.parseInt(input);
+                inputValidation.days(inputNumber);
                 validInputNumber = true;
+            } catch (NumberFormatException e) {
+                System.out.println("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.");
             }
         }
         return inputNumber;
