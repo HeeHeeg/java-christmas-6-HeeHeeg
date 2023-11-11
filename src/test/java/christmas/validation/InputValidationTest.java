2256a10b6a34d485b8ed50b7b644806eff2205f7
HeeHeeg <qmffndoden@naver.com>
2023-11-11 17:28:48 +0900

    test(christmas): 메뉴 수량이 1이상의 숫자가 아닌 경우 예외가 발생하는 테스트 코드 작성

--- src/test/java/christmas/validation/InputValidationTest.java
+++ src/test/java/christmas/validation/InputValidationTest.java
@@ -21,7 +21,7 @@ class InputValidationTest {
     @ParameterizedTest
     @ValueSource(ints = {0, 32, -1})
     void dateOutOfRange(int reservationDate) {
-        assertThatThrownBy(() -> inputValidation.validateDate(reservationDate))
+        assertThatThrownBy(() -> inputValidation.date(reservationDate))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.");
     }
@@ -30,7 +30,25 @@ class InputValidationTest {
     @ParameterizedTest
     @ValueSource(strings = {"숫자가아닌 값", "1j", " "})
     void visitDateIsNotNumber(String input) {
-        assertThatThrownBy(() -> inputParser.parseNumber(input))
+        assertThatThrownBy(() -> inputParser.dateParseNumber(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.");
+    }
+
+    @DisplayName("메뉴 수량이 1이상이 아니면 예외가 발생한다.")
+    @ParameterizedTest
+    @ValueSource(ints = {0, -1})
+    void checkMenuQuantity(int quantity) {
+        assertThatThrownBy(() -> inputValidation.menuQuantity(quantity))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
+    }
+
+    @DisplayName("메뉴 수량에 숫자가 아닌 값이 들어오면 예외가 발생한다.")
+    @ParameterizedTest
+    @ValueSource(strings = {"숫자가아닌 값", "1j", " "})
+    void checkMenuQuantityNotNumber(String input) {
+        assertThatThrownBy(() -> inputParser.dateParseNumber(input))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.");
     }
