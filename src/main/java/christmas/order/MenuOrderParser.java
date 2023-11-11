74d29df385fd2c0f8f2f219a15b8d457e4a1c525
HeeHeeg <qmffndoden@naver.com>
2023-11-11 17:26:16 +0900

    feat(christmas): 메뉴 개수가 1 이상의 숫자가 아닌 경우 예외처리 기능 구현

--- src/main/java/christmas/order/MenuOrderParser.java
+++ src/main/java/christmas/order/MenuOrderParser.java
@@ -2,12 +2,16 @@ package christmas.order;
 
 import christmas.menu.Menu;
 import christmas.menu.MenuItem;
+import christmas.validation.InputParser;
+import christmas.validation.InputValidator;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class MenuOrderParser {
+    private static final InputValidator inputValidation = new InputValidator();
+    private static final InputParser inputParser = new InputParser();
 
     public List<MenuItem> parseOrder(String inputMenu) {
         String[] items = inputMenu.split(",");
@@ -18,7 +22,8 @@ public class MenuOrderParser {
             if (!isValidMenu(menuName)) {
                 throw new IllegalArgumentException("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
             }
-            int quantity = Integer.parseInt(parts[1].trim());
+            int quantity = inputParser.menuQuantityParseNumber(parts[1].trim());
+            inputValidation.menuQuantity(quantity);
             orderedMenuList.add(new MenuItem(menuName, quantity));
         }
         return orderedMenuList;
--- src/main/java/christmas/validation/InputValidator.java
+++ src/main/java/christmas/validation/InputValidator.java
@@ -6,4 +6,10 @@ public class InputValidator {
             throw new IllegalArgumentException("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.");
         }
     }
+
+    public void menuQuantity(int quantity) {
+        if (!(quantity >= 1)) {
+            throw new IllegalArgumentException("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
+        }
+    }
 }
