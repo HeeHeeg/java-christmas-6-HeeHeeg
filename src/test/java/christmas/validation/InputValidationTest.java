7a8c6e0038f0e2dad61fb0a9473131d85753e992
HeeHeeg <qmffndoden@naver.com>
2023-11-11 09:03:45 +0900

    test(christmas): 1 이상 31 이하의 숫자가 아닌 경우 예외가 발생하는 테스트 코드 작성

---
+++ src/test/java/christmas/validation/InputValidationTest.java
@@ -0,0 +1,26 @@
+package christmas.validation;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class InputValidationTest {
+    private InputValidation inputValidation;
+
+    @BeforeEach
+    void setUp() {
+        inputValidation = new InputValidation();
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {0, 32, -1})
+    @DisplayName("방문할 날짜가 1 이상 31 이하가 아니면 예외가 발생한다.")
+    void dateOutOfRange(int reservationDate) {
+        assertThatThrownBy(() -> inputValidation.days(reservationDate))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.");
+    }
+}
