1d33af9f3fa645a324b36a850e1a8cee2e2e18bb
HeeHeeg <qmffndoden@naver.com>
2023-11-11 17:27:19 +0900

    feat(christmas): 메뉴 형식이 예시와 다른 경우 예외처리 기능 구현

--- src/main/java/christmas/validation/InputParser.java
+++ src/main/java/christmas/validation/InputParser.java
@@ -11,4 +11,14 @@ public class InputParser {
         }
         return inputNumber;
     }
+    public int menuQuantityParseNumber(String input) {
+        int menuQuantity = 0;
+        try {
+            menuQuantity = Integer.parseInt(input);
+
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
+        }
+        return menuQuantity;
+    }
 }
