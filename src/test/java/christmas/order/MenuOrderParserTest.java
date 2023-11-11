98a0e51bed207c6d279200ea2301c87d7564e4dd
HeeHeeg <qmffndoden@naver.com>
2023-11-11 22:40:41 +0900

    test(christmas): 메뉴를 ','와 '-'를 기준으로 분리해서 리스트에 담을 때 메뉴 수량이 1이상이 아니면 예외가 발생하는 테스트 코드 작성

--- src/test/java/christmas/order/MenuOrderParserTest.java
+++ src/test/java/christmas/order/MenuOrderParserTest.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.Test;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class MenuOrderParserTest {
     MenuOrderParser menuOrderParser;
@@ -32,5 +33,17 @@ class MenuOrderParserTest {
         assertThat(menuItems.get(0).getQuantity()).isEqualTo(2);
         assertThat(menuItems.get(1).getQuantity()).isEqualTo(1);
         assertThat(menuItems.get(2).getQuantity()).isEqualTo(1);
+    }
+
+    @DisplayName("메뉴 수량이 1이상이 아니면 예외가 발생한다.")
+    @Test
+    void checkMenuQuantity() {
+        // given
+        String inputMenu = "해산물파스타-0,레드와인-1,초코케이크-1";
+
+        // when-then
+        assertThatThrownBy(() ->  menuOrderParser.parseOrder(inputMenu))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
     }
 }
