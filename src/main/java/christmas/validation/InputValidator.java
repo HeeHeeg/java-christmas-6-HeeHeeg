db6d0635f5e9e24d045c3dda962acad616a78df8
HeeHeeg <qmffndoden@naver.com>
2023-11-11 14:39:21 +0900

    refactor(christmas): 변수명 변경

--- src/main/java/christmas/validation/InputValidator.java
+++ src/main/java/christmas/validation/InputValidator.java
@@ -1,7 +1,7 @@
 package christmas.validation;
 
-public class InputValidation {
-    public void days(int reservationDate) {
+public class InputValidator {
+    public void validateDate(int reservationDate) {
         if (!(reservationDate >= 1 && reservationDate <= 31)) {
             throw new IllegalArgumentException("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.");
         }
