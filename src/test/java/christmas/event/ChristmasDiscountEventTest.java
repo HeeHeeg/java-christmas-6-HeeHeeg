8e9f2a43dbcb7dd52362c32e15ce3f3058c6ffb2
HeeHeeg <qmffndoden@naver.com>
2023-11-12 20:39:39 +0900

    test(christmas): 예약일이 크리스마스 이벤트 기간이면 예약일에 해당하는 할인금액을 반환하는 테스트 코드 작성

--- src/test/java/christmas/event/ChristmasDiscountEventTest.java
+++ src/test/java/christmas/event/ChristmasDiscountEventTest.java
@@ -3,6 +3,7 @@ package christmas.event;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -22,5 +23,14 @@ class ChristmasDiscountEventTest {
         // when-then
         int calculatedDiscount = christmasDiscountEvent.calculateDiscount(date);
         assertThat(calculatedDiscount).isEqualTo(0);
+    }
+
+    @DisplayName("예약일이 크리스마스 할인 이벤트 기간이면 예약일에 해당하는 할인금액을 반환한다.")
+    @ParameterizedTest
+    @CsvSource({"1,1000", "15,2400", "25,3400"})
+    void reservationDateByChristmasDiscountEventPeriod(int date, int discountPrice) {
+        // when-then
+        int calculatedDiscount = christmasDiscountEvent.calculateDiscount(date);
+        assertThat(calculatedDiscount).isEqualTo(discountPrice);
     }
 }
