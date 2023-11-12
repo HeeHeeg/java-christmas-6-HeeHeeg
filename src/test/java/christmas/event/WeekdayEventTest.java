6c0ab79780eec70f36565f734e2f523d75d3cbc4
HeeHeeg <qmffndoden@naver.com>
2023-11-12 22:11:24 +0900

    test(christmas): 예약일이 주말이면 false를 반환하는 테스트 코드 작성

---
+++ src/test/java/christmas/event/WeekdayEventTest.java
@@ -0,0 +1,28 @@
+package christmas.event;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class WeekdayEventTest {
+    private WeekdayEvent weekdayEvent;
+
+    @BeforeEach
+    void setUp() {
+        weekdayEvent = new WeekdayEvent();
+    }
+
+    @DisplayName("예약일이 금요일 또는 토요일(주말)이면 false를 반환한다.")
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 8, 9}) // 2023년 12월의 금요일과 토요일 날짜
+    void reservationDateByNotWeekday(int date) {
+        // when
+        boolean checkWeekday = weekdayEvent.checkWeekdayDiscountPeriod(date);
+
+        // then
+        assertThat(checkWeekday).isFalse();
+    }
+}
