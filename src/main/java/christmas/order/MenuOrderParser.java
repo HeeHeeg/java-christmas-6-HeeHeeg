cf92b4438f32842deb51ccf19c5b29fd9d874158
HeeHeeg <qmffndoden@naver.com>
2023-11-12 17:04:18 +0900

    refactor(christmas): 음료만 주문시 예외처리하는 코드 위치 변경

--- src/main/java/christmas/order/MenuOrderParser.java
+++ src/main/java/christmas/order/MenuOrderParser.java
@@ -22,9 +22,9 @@ public class MenuOrderParser {
             int quantity = inputParser.menuQuantityParseNumber(parts[1].trim());
             inputValidator.menuQuantity(quantity);
             inputValidator.checkForDuplicateMenu(menuName, orderedMenuList);
-            inputValidator.checkOnlyBeveragesOrdered(orderedMenuList);
             orderedMenuList.add(new MenuItem(menuName, quantity));
         }
+        inputValidator.checkOnlyBeveragesOrdered(orderedMenuList);
         return orderedMenuList;
     }
 }
