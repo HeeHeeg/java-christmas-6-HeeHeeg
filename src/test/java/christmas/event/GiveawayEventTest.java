10befd520f8a6edaf5448fd6258196e25f143cff
HeeHeeg <qmffndoden@naver.com>
2023-11-13 23:05:18 +0900

    test(christmas): 증정이벤트-총주문 금액이 12만원 이상이면 샴페인을 1개 증정하는 테스트 코드 작성

---
+++ src/test/java/christmas/event/GiveawayEventTest.java
@@ -0,0 +1,36 @@
+package christmas.event;
+
+import christmas.menu.Menu;
+import christmas.menu.MenuItem;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static christmas.menu.Menu.*;
+import static org.assertj.core.api.Assertions.assertThat;
+
+class GiveawayEventTest {
+    private GiveawayEvent giveawayEvent;
+
+    @BeforeEach
+    void setUp() {
+        giveawayEvent = new GiveawayEvent();
+    }
+
+    @DisplayName("할인전 총주문 금액이 120,000원이 넘으면 증정품 샴페인을 반환한다.")
+    @Test
+    void checkGiveawayEvent() {
+        //given
+        int date = 3;
+        List<MenuItem> orderedItems = List.of(
+                new MenuItem(T_BONE_STEAK, 2),
+                new MenuItem(ICE_CREAM, 2));
+        //when
+        Menu giveawayMenu = giveawayEvent.checkGiveawayEvent(date, orderedItems);
+
+        //then
+        assertThat(giveawayMenu).isEqualTo(CHAMPAGNE);
+    }
+}
