184e28694c512b0b47c291e96e8488c5e646cac1
HeeHeeg <qmffndoden@naver.com>
2023-11-13 22:59:09 +0900

    refactor(christmas): 주문별 합계금액 계산하는 함수를 메뉴 클래스로 이동

--- src/main/java/christmas/Application.java
+++ src/main/java/christmas/Application.java
@@ -1,5 +1,6 @@
 package christmas;
 
+import christmas.calculation.Calculator;
 import christmas.menu.MenuItem;
 import christmas.view.InputView;
 import christmas.view.OutputView;
@@ -9,11 +10,13 @@ import java.util.List;
 public class Application {
     private static final InputView inputView = new InputView();
     private static final OutputView outputView = new OutputView();
+    private static final Calculator calculator = new Calculator();
 
     public static void main(String[] args) {
         inputView.readDate();
         List<MenuItem> orderedMenu = inputView.readMenu();
         outputView.printMenu(orderedMenu);
-        outputView.printTotalPrice(orderedMenu);
+        int totalPrice = calculator.calculateTotalPrice(orderedMenu);
+        outputView.printTotalPrice(totalPrice);
     }
 }
