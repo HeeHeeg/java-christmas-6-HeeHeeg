3150d61e6bf3f212d41e15cd6cde779c8095ccc6
HeeHeeg <qmffndoden@naver.com>
2023-11-13 23:07:05 +0900

    test(christmas): 증정이벤트-총주문 금액이 12만원을 넘지 않으면 null을 반환하는 테스트 코드 작성

--- src/test/java/christmas/event/GiveawayEventTest.java
+++ src/test/java/christmas/event/GiveawayEventTest.java
@@ -33,4 +33,19 @@ class GiveawayEventTest {
         //then
         assertThat(giveawayMenu).isEqualTo(CHAMPAGNE);
     }
+
+    @DisplayName("할인 전 총주문 금액이 120,000원이 넘지 않으면 null을 반환한다.")
+    @Test
+    void checkGiveawayEventNull() {
+        //given
+        int date = 3;
+        List<MenuItem> orderedItems = List.of(
+                new MenuItem(T_BONE_STEAK, 2),
+                new MenuItem(ICE_CREAM, 1));
+        //when
+        Menu giveawayMenu = giveawayEvent.checkGiveawayEvent(date, orderedItems);
+
+        //then
+        assertThat(giveawayMenu).isEqualTo(null);
+    }
 }
