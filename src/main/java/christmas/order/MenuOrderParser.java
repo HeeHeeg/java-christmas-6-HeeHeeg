fac34526b168ba28fa6431e2c18ff63a5ed5c46e
HeeHeeg <qmffndoden@naver.com>
2023-11-14 08:27:57 +0900

    refactor(christmas): 기능별로 메서드 분리

--- src/main/java/christmas/order/MenuOrderParser.java
+++ src/main/java/christmas/order/MenuOrderParser.java
@@ -14,19 +14,39 @@ public class MenuOrderParser {
 
     public List<MenuItem> parseOrder(String inputMenu) {
         String[] items = inputMenu.split(",");
-        List<MenuItem> orderedMenuList = new ArrayList<>();
-        for (String menuItem : items) {
-            String[] parts = menuItem.split("-");
-            inputValidator.checkMenuLength(parts);
-            String menuName = parts[0].trim();
-            inputValidator.checkMenuName(menuName);
-            int quantity = inputParser.menuQuantityParseNumber(parts[1].trim());
-            inputValidator.menuQuantity(quantity);
-            inputValidator.checkForDuplicateMenu(menuName, orderedMenuList);
-            orderedMenuList.add(new MenuItem(Menu.getMenuByName(menuName).orElse(null), quantity));
-        }
+        List<MenuItem> orderedMenuList = getOrderedMenuItems(items);
         inputValidator.checkOnlyBeveragesOrdered(orderedMenuList);
         inputValidator.checkOrderQuantity(orderedMenuList);
         return orderedMenuList;
+    }
+
+    private List<MenuItem> getOrderedMenuItems(String[] items) {
+        List<MenuItem> orderedMenuList = new ArrayList<>();
+        for (String menuItem : items) {
+            String[] parts = getParts(menuItem);
+            String menuName = getMenuName(parts);
+            int quantity = getQuantity(parts);
+            inputValidator.checkForDuplicateMenu(menuName, orderedMenuList);
+            orderedMenuList.add(new MenuItem(Menu.getMenuByName(menuName).orElse(null), quantity));
+        }
+        return orderedMenuList;
+    }
+
+    private String[] getParts(String menuItem) {
+        String[] parts = menuItem.split("-");
+        inputValidator.checkMenuLength(parts);
+        return parts;
+    }
+
+    private String getMenuName(String[] parts) {
+        String menuName = parts[0].trim();
+        inputValidator.checkMenuName(menuName);
+        return menuName;
+    }
+
+    private int getQuantity(String[] parts) {
+        int quantity = inputParser.menuQuantityParseNumber(parts[1].trim());
+        inputValidator.menuQuantity(quantity);
+        return quantity;
     }
 }
