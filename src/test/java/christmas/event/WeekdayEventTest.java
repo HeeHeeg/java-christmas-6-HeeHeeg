ec2c5da7dedad8a028c6fb096532011c8a510938
HeeHeeg <qmffndoden@naver.com>
2023-11-13 10:12:40 +0900

    test(christmas): 평일 디저트 할인에 대한 테스트 코드 변경

--- src/test/java/christmas/event/WeekdayEventTest.java
+++ src/test/java/christmas/event/WeekdayEventTest.java
@@ -1,9 +1,13 @@
 package christmas.event;
 
+import christmas.menu.Menu;
+import christmas.menu.MenuItem;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -15,25 +19,35 @@ class WeekdayEventTest {
         weekdayEvent = new WeekdayEvent();
     }
 
-    @DisplayName("예약일이 금요일 또는 토요일(주말)이면 false를 반환한다.")
+    @DisplayName("예약일이 금요일 또는 토요일(주말)이면 디저트 할인 금액 0원을 반환한다.")
     @ParameterizedTest
     @ValueSource(ints = {1, 2, 8, 9}) // 2023년 12월의 금요일과 토요일 날짜
     void reservationDateByNotWeekday(int date) {
+        // given
+        List<MenuItem> orderedItems = List.of(
+                new MenuItem(Menu.getMenuByName("해산물파스타").orElse(null), 1),
+                new MenuItem(Menu.getMenuByName("아이스크림").orElse(null), 2));
+
         // when
-        boolean checkWeekday = weekdayEvent.checkWeekdayDiscountPeriod(date);
+        int discountedPrice = weekdayEvent.calculateDessertDiscount(orderedItems, date);
 
         // then
-        assertThat(checkWeekday).isFalse();
+        assertThat(discountedPrice).isEqualTo(0);
     }
 
-    @DisplayName("예약일이 일요일~목요일(평일)이면 true를 반환한다.")
+    @DisplayName("예약일이 일요일~목요일(평일)이면 주문 메뉴에서 디저트 메뉴 1개당 2,023원씩 할인한 값을 계산해 할인금액을 반환한다.")
     @ParameterizedTest
-    @ValueSource(ints = {3, 4, 5, 6, 7}) // 2023년 12월의 일요일~목요일 날짜
-    void reservationDateWeekdays(int date) {
+    @ValueSource(ints = {3, 4, 5, 6, 7})  // 2023년 12월의 일요일~목요일 날짜
+    void reservationDateWeekdaysDiscountPerDessert(int date) {
+        // given
+        List<MenuItem> orderedItems = List.of(
+                new MenuItem(Menu.getMenuByName("해산물파스타").orElse(null), 1),
+                new MenuItem(Menu.getMenuByName("아이스크림").orElse(null), 2));
+
         // when
-        boolean checkWeekday = weekdayEvent.checkWeekdayDiscountPeriod(date);
+        int discountedPrice = weekdayEvent.calculateDessertDiscount(orderedItems, date);
 
         // then
-        assertThat(checkWeekday).isTrue();
+        assertThat(discountedPrice).isEqualTo(4046);
     }
 }
